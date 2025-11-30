b119bf9e79e5fa5f777d4d38f51568c63f8250a9
Vinoth <vinothnimsara00@gmail.com>
2025-11-29 21:58:43 -0500

    Fix secrets_example.h for CI compatibility

--- include/secrets_example.h
+++ include/secrets_example.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#define WIFI_SSID      "dummy"
-#define WIFI_PASSWORD  "dummy_password"
-#define MQTT_BROKER    "dummy_broker"
-#define MQTT_PORT      1883
-#define MQTT_TOPIC     "dummy/topic"
+// Dummy fallback values for CI builds
+
+#define WIFI_SSID_ENV     "dummy_ssid"
+#define WIFI_PASS_ENV     "dummy_password"
+
+#define MQTT_BROKER_ENV   "dummy_broker"
+#define MQTT_PORT_ENV     1883
+#define MQTT_TOPIC_ENV    "dummy/topic"
+
